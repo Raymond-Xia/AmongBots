ad42744650212035885f8784ceb7165de9988d05
RAYMOND XIA <USERNAME@uwaterloo.ca>
2022-06-26 22:00:41 +0100

    Added 3 answer buttons with one correct answer

--- Assets/QuestionGenerator.cs
+++ Assets/QuestionGenerator.cs
@@ -6,18 +6,24 @@ using TMPro;
 public class QuestionGenerator : MonoBehaviour
 {
     public TMP_Text question;
-    public int min, max;
+    public TMP_Text a1;
+    public TMP_Text a2;
+    public TMP_Text a3;
+    public int minNum, maxNum;
     public int X, Y;
 
     // Start is called before the first frame update
     void Start()
     {
         question = GetComponent<TMP_Text>();
-        min = 0;
-        max = 10;
-        X = Random.Range(min,max);
-        Y = Random.Range(min,max);
+        a1 = GameObject.Find("A1Text").GetComponent<TMP_Text>();
+        minNum = 0;
+        maxNum = 10;
+        X = Random.Range(minNum,maxNum);
+        Y = Random.Range(minNum,maxNum);
+        int answer = X + Y;
         question.text = X + " + " + Y + " = ?";
+        a1.text = answer + "";
     }
 
 }
