ae76af793884a973c7c0e227afdacee3b391b4e1
Daniel Kim <daniel.k.0710@gmail.com>
2022-07-17 18:27:02 -0400

    created generic class for attacks

--- Assets/Scripts/Attacks/AttackHoming.cs
+++ Assets/Scripts/Attacks/AttackHoming.cs
@@ -1,31 +1,8 @@
 using System.Collections;
 using UnityEngine;
 
-public class AttackHoming : MonoBehaviour
+public class AttackHoming : Attack
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.y < 0f || transform.position.y > Screen.height || transform.position.x > Screen.width || transform.position.x < 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == Constants.PLAYER_TAG)
-        {
-            Destroy(gameObject);
-        }
-    }
-
     public static void ShootOnDemand(GameObject enemy, GameObject missile, float shootSpeed, Transform shootPos, Transform playerPos) 
     {
         GameObject newMissile = Instantiate(missile, shootPos.position, Quaternion.identity) as GameObject;
--- Assets/Scripts/Attacks/AttackSpiral.cs
+++ Assets/Scripts/Attacks/AttackSpiral.cs
@@ -2,34 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackSpiral : MonoBehaviour
+public class AttackSpiral : Attack
 {
-    // Start is called before the first frame update
-    public float damage;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.y < 0f || transform.position.y > Screen.height || transform.position.x > Screen.width || transform.position.x < 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == Constants.PLAYER_TAG)
-        {
-            Destroy(gameObject);
-        }
-    }
-
     public static IEnumerator ShootInWaves(GameObject enemy, GameObject missile, float delay, int ammo, float shootSpeed, Transform shootPos)
     {
         while (ammo > 0)
