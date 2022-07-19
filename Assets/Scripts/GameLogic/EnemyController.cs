fab03b02e66a6e3b29a0999bf54f2c66d2ecf7e0
Daniel Kim <daniel.k.0710@gmail.com>
2022-07-19 01:56:49 -0400

    9/9

--- Assets/Scripts/GameLogic/EnemyController.cs
+++ Assets/Scripts/GameLogic/EnemyController.cs
@@ -58,14 +58,6 @@ public class EnemyController : MonoBehaviour
     {
         switch(level) 
         {
-            case > 30:
-                if (level % 3 == 0)
-                    StartCoroutine(Generate5Waves_A());
-                else if (level % 3 == 1)
-                    StartCoroutine(Generate5Waves_B());
-                else
-                    StartCoroutine(Generate5Waves_C());
-                break;
             case > 20:
                 if (level % 3 == 0) {
                     enemiesThisRound = 24;
@@ -75,8 +67,10 @@ public class EnemyController : MonoBehaviour
                     enemiesThisRound = 14;
                     StartCoroutine(Generate4Waves_B());
                 }
-                else
+                else {
+                    enemiesThisRound = 35;
                     StartCoroutine(Generate4Waves_C());
+                }
                 break;
             case > 10:
                 if (level % 3 == 0) {
@@ -549,32 +543,178 @@ public class EnemyController : MonoBehaviour
     IEnumerator Generate4Waves_C() 
     {
         enemiesSpawned = 0;
-        int enemiesThisWave = 3;
+        int enemiesThisWave = 2;
         EnemyParameters[] e = new EnemyParameters[enemiesThisWave];
-        yield return new WaitUntil(new System.Func<bool>(() => enemiesDespawned == enemiesSpawned));
-    }
 
-    IEnumerator Generate5Waves_A() 
-    {
-        enemiesSpawned = 0;
-        int enemiesThisWave = 3;
-        EnemyParameters[] e = new EnemyParameters[enemiesThisWave];
-        yield return new WaitUntil(new System.Func<bool>(() => enemiesDespawned == enemiesSpawned));
-    }
+        for (int i = 0; i < enemiesThisWave; i++) 
+        {
+            e[i] = new EnemyParameters(
+                80000f, 30000f, 500, Constants.VERTICAL_ATTACK, 
+                new Vector2((1 + 4*i) * Screen.width / 6, 11*Screen.height / 12), 
+                new Vector2((1 + 4*i) * Screen.width / 6, Screen.height), 
+                new Vector2((1 + 4*i) * Screen.width / 6, Screen.height),
+                Constants.PAUSE_TO_SHOOT, 0.01f, 1, 1
+            );
+        }
+        enemiesSpawned += enemiesThisWave;
+        SpawnEnemiesSynchronous(enemiesThisWave, e);
 
-    IEnumerator Generate5Waves_B() 
-    {
-        enemiesSpawned = 0;
-        int enemiesThisWave = 3;
-        EnemyParameters[] e = new EnemyParameters[enemiesThisWave];
-        yield return new WaitUntil(new System.Func<bool>(() => enemiesDespawned == enemiesSpawned));
-    }
+        enemiesThisWave = 1;
+        e = new EnemyParameters[]{new EnemyParameters(
+            80000f, 30000f, 8, Constants.CIRCLE_AND_HOMING_ATTACK, 
+            new Vector2(Screen.width / 2, 2*Screen.height / 3), 
+            new Vector2(Screen.width / 2, Screen.height), 
+            new Vector2(Screen.width / 2, Screen.height),
+            Constants.PAUSE_TO_SHOOT, 0.5f, 1, 1
+        )};
+        enemiesSpawned += enemiesThisWave;
+        SpawnEnemiesSynchronous(enemiesThisWave, e);
 
-    IEnumerator Generate5Waves_C() 
-    {
-        enemiesSpawned = 0;
-        int enemiesThisWave = 3;
-        EnemyParameters[] e = new EnemyParameters[enemiesThisWave];
+        yield return new WaitUntil(new System.Func<bool>(() => enemiesDespawned == enemiesSpawned));
+
+        enemiesThisWave = 4;
+        e = new EnemyParameters[enemiesThisWave];
+        for (int i = 0; i < 2; i++) 
+        {
+            e[i] = new EnemyParameters(
+                80000f, 30000f, 500, Constants.VERTICAL_ATTACK, 
+                new Vector2((1 + i) * Screen.width / 7, 11*Screen.height / 12), 
+                new Vector2((1 + i) * Screen.width / 7, Screen.height), 
+                new Vector2((1 + i) * Screen.width / 7, Screen.height),
+                Constants.PAUSE_TO_SHOOT, 0.01f, 1, 1
+            );
+        }
+        for (int i = 3; i <= enemiesThisWave; i++) 
+        {
+            e[i-1] = new EnemyParameters(
+                80000f, 30000f, 500, Constants.VERTICAL_ATTACK, 
+                new Vector2((2 + i) * Screen.width / 7, 11*Screen.height / 12), 
+                new Vector2((2 + i) * Screen.width / 7, Screen.height), 
+                new Vector2((2 + i) * Screen.width / 7, Screen.height),
+                Constants.PAUSE_TO_SHOOT, 0.01f, 1, 1
+            );
+        }
+        enemiesSpawned += enemiesThisWave;
+        SpawnEnemiesSynchronous(enemiesThisWave, e);
+
+        yield return new WaitForSeconds(1.5f);
+
+        enemiesThisWave = 8;
+        e = new EnemyParameters[enemiesThisWave];
+        for (int i = 0; i < enemiesThisWave; i++) 
+        {
+            System.Random r1 = new System.Random();
+            int pointPair = r1.Next(0, 6);
+
+            System.Random r2 = new System.Random();
+            int forwards = r2.Next(0, 2);
+
+            if (forwards == 1) 
+            {
+                e[i] = new EnemyParameters(
+                    40000f, 60000f, 1, Constants.NO_ATTACK, 
+                    fixedExitLocations[pointPair * 2], 
+                    fixedSpawnLocations[pointPair * 2], 
+                    fixedExitLocations[pointPair * 2],
+                    Constants.SHOOT_AND_FLY, 1, 1, 1
+                );
+            }
+            else 
+            {
+                e[i] = new EnemyParameters(
+                    40000f, 60000f, 1, Constants.NO_ATTACK, 
+                    fixedSpawnLocations[pointPair * 2], 
+                    fixedExitLocations[pointPair * 2], 
+                    fixedSpawnLocations[pointPair * 2],
+                    Constants.SHOOT_AND_FLY, 1, 1, 1
+                );
+            }
+        }
+        enemiesSpawned += enemiesThisWave;
+        StartCoroutine(SpawnEnemiesWithDelay(enemiesThisWave, e));
+        yield return new WaitUntil(new System.Func<bool>(() => enemiesDespawned == enemiesSpawned));
+
+        enemiesThisWave = 2;
+        e = new EnemyParameters[enemiesThisWave];
+        for (int i = 0; i < enemiesThisWave; i++) 
+        {
+            e[i] = new EnemyParameters(
+                40000f, 30000f, 5, Constants.CIRCLE_AND_HOMING_ATTACK, 
+                new Vector2((1 + 4*i) * Screen.width / 6, 5* Screen.height / 6), 
+                new Vector2((1 + 4*i) * Screen.width / 6, Screen.height), 
+                new Vector2((1 + 4*i) * Screen.width / 6, Screen.height),
+                Constants.PAUSE_TO_SHOOT, 0.8f, 1, 1
+            );
+        }
+        enemiesSpawned += enemiesThisWave;
+        SpawnEnemiesSynchronous(enemiesThisWave, e);
+
+        e = new EnemyParameters[enemiesThisWave];
+        for (int i = 0; i < enemiesThisWave; i++) 
+        {
+            e[i] = new EnemyParameters(
+                40000f, 30000f, 5, Constants.CIRCLE_AND_HOMING_ATTACK, 
+                new Vector2((1 + 4*i) * Screen.width / 6, Screen.height / 6), 
+                new Vector2((1 + 4*i) * Screen.width / 6, 0), 
+                new Vector2((1 + 4*i) * Screen.width / 6, 0),
+                Constants.PAUSE_TO_SHOOT, 0.8f, 1, 1
+            );
+        }
+        enemiesSpawned += enemiesThisWave;
+        SpawnEnemiesSynchronous(enemiesThisWave, e);
+        yield return new WaitUntil(new System.Func<bool>(() => enemiesDespawned == enemiesSpawned));
+
+        enemiesThisWave = 16;
+        e = new EnemyParameters[enemiesThisWave];
+        for (int i = 0; i < enemiesThisWave; i++) 
+        {
+            System.Random r1 = new System.Random();
+            int pointPair = r1.Next(0, 13);
+
+            System.Random r2 = new System.Random();
+            int forwards = r2.Next(0, 2);
+            Vector2 pausePoint = new Vector2(0,0);
+            if (forwards == 1) 
+            {
+                
+                if (fixedSpawnLocations[pointPair].x == 0) 
+                {
+                    pausePoint = new Vector2(Screen.width / 6, fixedSpawnLocations[pointPair].y);
+                }
+                else if (fixedSpawnLocations[pointPair].y == Screen.height) 
+                {
+                    pausePoint = new Vector2(fixedSpawnLocations[pointPair].x, 5*Screen.height / 6);
+                }
+
+                e[i] = new EnemyParameters(
+                    40000f, 60000f, 1, Constants.RANDOM_AND_HOMING_ATTACK, 
+                    pausePoint, 
+                    fixedSpawnLocations[pointPair], 
+                    fixedSpawnLocations[pointPair],
+                    Constants.PAUSE_TO_SHOOT, 1, 1, 1
+                );
+            }
+            else 
+            {
+                if (fixedExitLocations[pointPair].x == Screen.width) 
+                {
+                    pausePoint = new Vector2(5 * Screen.width / 6, fixedExitLocations[pointPair].y);
+                }
+                else if (fixedSpawnLocations[pointPair].y == 0) 
+                {
+                    pausePoint = new Vector2(fixedExitLocations[pointPair].x, Screen.height / 6);
+                }
+                e[i] = new EnemyParameters(
+                    40000f, 60000f, 1, Constants.RANDOM_AND_HOMING_ATTACK, 
+                    pausePoint, 
+                    fixedExitLocations[pointPair], 
+                    fixedExitLocations[pointPair],
+                    Constants.PAUSE_TO_SHOOT, 1, 1, 1
+                );
+            }
+        }
+        enemiesSpawned += enemiesThisWave;
+        StartCoroutine(SpawnEnemiesWithDelay(enemiesSpawned, e));
         yield return new WaitUntil(new System.Func<bool>(() => enemiesDespawned == enemiesSpawned));
     }
 }
