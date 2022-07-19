f76c6e87c0e89093bfea954774318047f03d5f9e
RAYMOND XIA <USERNAME@uwaterloo.ca>
2022-07-20 00:37:05 +0100

    constants git statusgit status!

--- Assets/Scripts/Menu/VolumeSlider.cs
+++ Assets/Scripts/Menu/VolumeSlider.cs
@@ -10,7 +10,7 @@ public class VolumeSlider : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        controller = GameObject.Find("AudioController").GetComponent<AudioController>();
+        controller = GameObject.Find(Constants.AUDIO_CONTROLLER_OBJECT).GetComponent<AudioController>();
         Slider slider = gameObject.GetComponent<Slider>();
         AudioController.volumeSlider = gameObject.GetComponent<Slider>();
         slider.value = AudioController.volume;
